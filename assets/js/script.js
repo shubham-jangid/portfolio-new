37b1d0b57fea8fb052b8b5b45946d7c109500679
shubham jangid <sjangid@tekion.com>
2023-06-30 17:21:18 +0530

    added contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,39 +14,78 @@ sidebarBtn.addEventListener("click", function () {
   elementToggleFunc(sidebar);
 });
 
-// contact form variables
-const form = document.querySelector("[data-form]");
-const formInputs = document.querySelectorAll("[data-form-input]");
-const formBtn = document.querySelector("[data-form-btn]");
+function submitForm(event) {
+  event.preventDefault();
+  const form = document.getElementById("form");
+  const nameInput = document.getElementById("fullName");
+  const emailInput = document.getElementById("email");
+  const messageInput = document.getElementById("message");
+  const successMessageElement = document.getElementById("successMessage");
+  const formButtonIconElement = document.getElementById("formButtonIcon");
+  const formSubmitBtnElement = document.getElementById("formSubmitBtn");
 
-// add event to all form input field
-for (let i = 0; i < formInputs.length; i++) {
-  formInputs[i].addEventListener("input", function () {
-    // check form validation
-    if (form.checkValidity()) {
-      formBtn.removeAttribute("disabled");
-    } else {
-      formBtn.setAttribute("disabled", "");
-    }
-  });
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const message = messageInput.value;
+
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    return;
+  }
+
+  formButtonIconElement.setAttribute("name", "refresh-outline");
+  formSubmitBtnElement.classList.add("formDisabledSubmitBtn");
+  formButtonIconElement.classList.add("loading-spinner");
+
+  fetch("https://formsubmit.co/contact@subhamjangid.in", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+      message,
+    }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        successMessageElement.innerText =
+          "Thank you for your submission! I will be in touch soon.";
+        form.reset();
+      } else {
+        throw new Error("Error submitting form.");
+      }
+    })
+    .catch((error) => {
+      successMessageElement.innerText(error);
+    })
+    .finally(() => {
+      formButtonIconElement.setAttribute("name", "paper-plane");
+      formSubmitBtnElement.classList.remove("formDisabledSubmitBtn");
+      formButtonIconElement.classList.remove("loading-spinner");
+    });
 }
 
 // page navigation variables
+
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
+const handleActivePage = function () {
+  for (let i = 0; i < pages.length; i++) {
+    if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+      pages[i].classList.add("active");
+      navigationLinks[i].classList.add("active");
+      window.scrollTo(0, 0);
+    } else {
+      pages[i].classList.remove("active");
+      navigationLinks[i].classList.remove("active");
+    }
+  }
+};
+
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
-      } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
-      }
-    }
-  });
+  navigationLinks[i].addEventListener("click", handleActivePage);
 }
